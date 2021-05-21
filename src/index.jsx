96ab5052d9b2302b5d71c95f8861be502fb78295
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2021-05-21 17:31:16 +0200

    router

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import { render } from 'react-dom';
 import './style.css';
 import { Header } from './components/Header/index';
 import { Footer } from './components/Footer/index';
+import { Home } from './components/Home/index';
+import { Reservation } from './components/Reservation/index';
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <Link to="/">Home</Link>
+        <Link to="/reservation">Reservation</Link>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/reservation">
+            <Reservation />
+          </Route>
+        </Switch>
+        <Footer />
+      </>
+    </Router>
   );
 };
 
