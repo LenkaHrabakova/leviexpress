a8b06f973e0b166a522ea7beebe15c6c4b855cc3
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2021-05-21 18:07:34 +0200

    5.1 - no link to API

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { JourneyPicker } from '../JourneyPicker';
 
 export const Home = () => {
   return (
     <>
       <h2>Home</h2>
+      <JourneyPicker />
     </>
   );
 };
